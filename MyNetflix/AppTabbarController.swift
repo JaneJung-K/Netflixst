db7565e805850e618822b72afe25c7bb9506d67b
JaneJung <piaojane@daum.net>
2021-02-09 13:18:33 +0900

    Initial Commit

---
+++ MyNetflix/AppTabbarController.swift
@@ -0,0 +1,19 @@
+
+
+import UIKit
+
+class AppTabbarController: UITabBarController {
+    
+   override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ MyNetflix/PlayerView.swift
@@ -0,0 +1,26 @@
+
+
+// https://developer.apple.com/documentation/avfoundation/avplayerlayer
+
+import UIKit
+import AVFoundation
+
+class PlayerView: UIView {
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    // Override UIView property
+    override static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+}
